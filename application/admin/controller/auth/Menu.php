984cf2bd26f41dfc65699da03882cc2b5f99f189
jim <1255905516@qq.com>
2020-07-19 20:57:06 +0800

    菜单-未完成

---
+++ application/admin/controller/auth/Menu.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User：Jim.
+ * Date：2020/7/19
+ * Time：17:43
+ */
+
+namespace app\admin\controller\auth;
+
+
+use app\common\model\Backend;
+
+class Menu extends Backend
+{
+
+}
