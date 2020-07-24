0ef3b9f4c420a6f6c4706d183ef2ce085c87510f
Jim <1255905516@qq.com>
2020-07-24 17:24:31 +0800

    api libs traits/Api.php 完善

--- application/api/libs/traits/Api.php
+++ application/api/libs/traits/Api.php
@@ -8,6 +8,8 @@
 
 namespace app\api\libs\traits;
 
+use app\api\libs\auth\Auth;
+
 /**
  * API 基础
  * Trait Api
@@ -15,5 +17,43 @@ namespace app\api\libs\traits;
  */
 trait Api
 {
+
+
+    /**
+     * 无需登录的方法,同时也就不需要鉴权了
+     * @var array
+     */
+    public $noNeedLogin = [];
+
+    /**
+     * 无需鉴权的方法,但需要登录
+     * @var array
+     */
+    public $noNeedRight = [];
+
+    /**
+     * 权限控制类
+     * @var Auth
+     */
+    public $auth = null;
+
+    /**
+     * 模型对象
+     * @var \think\Model
+     */
+    public $model = null;
+
+    /**
+     * Validate验证
+     */
+    public $_validate = false;
+
+    /**
+     * 是否开启数据限制
+     * 默认为禁用,若启用请务必保证表中存在admin_id字段
+     */
+    public $dataLimit = false;
+
+
 
 }
